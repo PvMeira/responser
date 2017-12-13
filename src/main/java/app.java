9724101d5f316e79467d8dc5f31e1b886a671b86
dfcamargo <dayan.camargo@gmail.com>
2017-12-13 18:55:16 -0200

    add some tests; created step builder;

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,19 +1,39 @@
+import com.batata.responser.exception.model.Error;
 import com.batata.responser.model.ResponseJson2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class app {
     public static void main(String[] args){
 
         System.out.println("init");
         ResponseJson2<String, String> meh;
-        meh = new ResponseJson2.ResponseBuilder()
-                               .withBody(new ArrayList())
+        meh = new ResponseJson2.ContentBuilder<String, String>()
+                               .withBody("Batata")
                                .withMeta("meh")
                                .create();
+        System.out.println("Teste1:" + meh);
+
+        List<Error> meee = new ArrayList();
+        meee.add(new Error("some error"));
+        meee.add( new Error("another error"));
+
+
+        meh = new ResponseJson2.ContentBuilder<String, String>()
+                .withErrors(meee.toArray(new Error[meee.size()]))
+                .withMeta(new Error("meeeeeeeeeeeeeeeeh"))
+                .create();
+        System.out.println("\n\nTeste2:" + meh);
 
 
 
+        meh = new ResponseJson2.ContentBuilder<String, String>()
+                .withErrors(meee.toArray(new Error[meee.size()]))
+                .withBody("somenthing")
+                .withMeta(new Error("meeeeeeeeeeeeeeeeh"))
+                .create();
+        System.out.println("\n\nTeste3:" +meh);
     }
 
 }
--- src/main/java/com/batata/responser/model/ResponseJson2.java
+++ src/main/java/com/batata/responser/model/ResponseJson2.java
@@ -4,6 +4,7 @@ import com.batata.responser.exception.model.Error;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,35 +17,42 @@ import java.util.List;
  */
 //TODO rename it :)
 public class ResponseJson2<B, M> {
-    private List<B> data;
-    private List<Error> errors;
-    private M meta;
+    private final List<B> data;
+    private final List<Error> errors;
+    private final M meta;
 
-    private ResponseJson2(ResponseBuilder builder){
+    private ResponseJson2(ContentBuilder builder){
         this.data   = builder.data;
         this.errors = builder.errors;
         //TODO is this really necessary?
         this.meta   = (builder.meta != null ?  (M) builder.meta : null);
     }
 
-    /**
-     * Builder inner class
-     * @param <B> A body value
-     * @param <M> A meta value (optional)
-     */
-    public static class ResponseBuilder<B, M> {
+    public static interface ContentStep<B> {
+        Build withoutContent();
+        Build withBody(B body);
+        Build withErrors(Error... errors);
+    }
+
+    public static interface Build<M>  {
+        ResponseJson2 create();
+        Build withMeta(@NotNull M meta);
+
+    }
+
+    public static class ContentBuilder<B, M> implements Build<M>, ContentStep<B> {
         private List<B> data;
         private List<Error> errors;
         private M meta;
 
-        public ResponseBuilder withoutContent(){
+        public ContentBuilder withoutContent(){
             this.data   = null;
             this.errors = null;
             this.meta   = null;
             return  this;
         }
 
-        public ResponseBuilder withBody(@NotNull B body){
+        public ContentBuilder withBody(@NotNull B body){
             data =  new ArrayList<>();
             if(body != null) {
                 if(body instanceof Collections)
@@ -55,13 +63,13 @@ public class ResponseJson2<B, M> {
             return this;
         }
 
-        public ResponseBuilder withErrors(@NotNull List<Error> errors){
+        public ContentBuilder withErrors(@NotNull Error... errors){
             data = null;
-            this.errors = errors;
+            this.errors = new ArrayList<Error>(Arrays.asList(errors));
             return this;
         }
 
-        public ResponseBuilder withMeta(@NotNull M meta) {
+        public Build withMeta(@NotNull M meta) {
             this.meta = meta;
             return this;
         }
